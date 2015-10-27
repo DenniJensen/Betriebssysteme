ac1bdc15dcd6e9ec89f6e80090da68b2ad62989f
Dennis Haegler <d.haegler@hotmail.de>
2015-10-27 17:16:58 +0100

    Add more for task 1.

--- io_task.c
+++ io_task.c
@@ -1,39 +1,41 @@
+#include <stdarg.h>
 
-#define DBGU 0xFFFFF200
+#define DBGU 0xFFFFF200 // 512 Bytes
 
 #define DBGU_CR 0x00
 #define DBGU_SR 0x14
 #define DBGU_RHR 0x18
 #define DBGU_THR 0x1C
 
-#define TURN_ON 1
+#define TURN_ON 0x01
 
-#define RXEN 4
-#define TXEN 6
+#define RXEN 0x04
+#define TXEN 0x06
+
+static inline unsigned int get_data(unsigned int addr)
+{
+  return *(volatile unsigned int *)addr;
+}
 
 static inline void write_u32(unsigned int addr, unsigned int val)
 {
-  *(volatile unsigned int *)addr = val;
-}
-
-static inline void read(unsigned int addr, volatile unsigned int* val)
-{
-  *(volatile unsigned int *)addr = (unsigned int)&(*(val));
+  // change to bit not int
+  *(volatile unsigned char *)addr = val;
 }
 
 void input_output(void)
 {
-// Receiver anschalten
-write_u32(DBGU + DBGU_CR + RXEN, TURN_ON);
-// Transmitter aktivieren
-// Bad offset
-// write_u32(DBGU + DBGU_CR + TXEN, TURN_ON);
+  // Receiver anschalten
+  write_u32(DBGU + DBGU_CR, RXEN);
 
-// in THR schreiben
-write_u32(DBGU + DBGU_THR, 'h');
-write_u32(DBGU + DBGU_THR, 'a');
-write_u32(DBGU + DBGU_THR, 'c');
-write_u32(DBGU + DBGU_THR, (unsigned int)&(*((volatile unsigned int *)DBGU + DBGU_SR)));
+  // Transmitter aktivieren
+  // Bad offset
+  write_u32(DBGU + DBGU_CR, TXEN);
 
+  // in THR schreiben
+  write_u32(DBGU + DBGU_THR, 'h');
+  write_u32(DBGU + DBGU_THR, 'a');
+
+  write_u32(DBGU + DBGU_THR, (DBGU + DBGU_RHR));
 }
 
